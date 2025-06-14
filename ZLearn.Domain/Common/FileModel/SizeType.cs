fa4b9997d00f7dee5de370b47e3b070e504a9797
zg04ckpt <nguyenhc424@gmail.com>
2025-06-14 16:02:29 +0700

    add file system model

---
+++ ZLearn.Domain/Common/FileModel/SizeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZLearn.Domain.Common.FileModel
+{
+    public enum SizeType
+    {
+        Mb,
+        Gb,
+        Byte,
+        Kb
+    }
+}
